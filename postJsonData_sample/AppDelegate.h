bfc38ba48d200a4099b66d7225aa6899d48efacb
yuanyu.chang <yuanyu.chang@coretronic.com>
2014-04-24 14:59:38 +0800

    Initial Commit

---
+++ postJsonData_sample/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  postJsonData_sample
+//
+//  Created by Chang YuanYu on 2014/4/24.
+//  Copyright (c) 2014年 James. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ postJsonData_sample/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  postJsonData_sample
+//
+//  Created by Chang YuanYu on 2014/4/24.
+//  Copyright (c) 2014年 James. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end

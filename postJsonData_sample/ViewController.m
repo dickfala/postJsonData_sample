2703555501cb11957cba81a9eff05f6f18808b0f
yuanyu.chang <yuanyu.chang@coretronic.com>
2014-06-13 09:45:42 +0800

    add code

--- postJsonData_sample/ViewController.m
+++ postJsonData_sample/ViewController.m
@@ -8,8 +8,13 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+static NSString *const BaseURLString =
+@"http://youtIP.php";
 
+@interface ViewController ()
+{
+    NSMutableData *getResponseData;
+}
 @end
 
 @implementation ViewController
@@ -17,13 +22,61 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+
+    [self loadWebserverData];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+
+// use xcode kit
+-(void) loadWebserverData
+{
+    
+    NSURL *url = [NSURL URLWithString:BaseURLString];
+    
+    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:url];
+    
+    
+    NSString *jsonContent = @"hello world";
+    // NSDictionary *parameters = @{@"data" : @"hello world"};
+    //{"data" = "your message"}
+    NSString *bodydata=[NSString stringWithFormat:@"data=%@",jsonContent];
+    [request setHTTPMethod:@"POST"];
+    NSData *req=[NSData dataWithBytes:[bodydata UTF8String] length:[bodydata length]];
+    
+    
+    [request setHTTPBody:req];
+    
+    NSURLConnection *connection = [[NSURLConnection alloc]initWithRequest:request delegate:self];
+    
+    if( connection )
+    {
+        getResponseData = [NSMutableData new];
+    }
+}
+
+
+// callback -NSURLConnection
+-(void) connection:(NSURLConnection*) connetion didReceiveData:(NSData *)data
+{
+    [getResponseData appendData:data];
+}
+
+-(void) connection:(NSURLConnection*)connection didFailWithError:(NSError *)error
+{
+    NSLog(@"error: %@", [error localizedDescription]);
+}
+
+-(void) connectionDidFinishLoading:(NSURLConnection*) connection
+{
+    NSLog(@"請求完成");
+    NSDictionary *dict = [NSJSONSerialization JSONObjectWithData:getResponseData options:NSJSONReadingAllowFragments error:nil];
+    NSLog(@"取得return資料: %@", dict);
 }
 
 @end
